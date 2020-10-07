17c3d54ae721081c33fa73985eb53cc66322c4dc
Sammy Alvarado <isammyalvarado@gmail.com>
2020-10-06 21:47:34 -0400

    created initial project
    Added framework for new project.

---
+++ LoadingUI/LoadingUI/LoadingUI.h
@@ -0,0 +1,18 @@
+//
+//  LoadingUI.h
+//  LoadingUI
+//
+//  Created by Sammy Alvarado on 10/6/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoadingUI.
+FOUNDATION_EXPORT double LoadingUIVersionNumber;
+
+//! Project version string for LoadingUI.
+FOUNDATION_EXPORT const unsigned char LoadingUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoadingUI/PublicHeader.h>
+
+
